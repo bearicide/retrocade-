2f2ee3e87e04d8f1418ec22d0be3d1980f689af7
MATTBEAR <matt.baier86@gmail.com>
2026-05-15 12:08:06 -0400

    Add RETROCADE service worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME='retrocade-v1';
+const ASSETS=[
+ './',
+ './index.html',
+ './manifest.json',
+ './assets/branding/retrocade-logo.svg'
+];
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+ self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+ event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+ );
+ self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+ event.respondWith(
+  caches.match(event.request).then(response=>response||fetch(event.request))
+ );
+});
