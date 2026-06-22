629f4bb94ab2f05f546032e2059cdfa1fc1e0933
MATTBEAR <matt.baier86@gmail.com>
2026-06-22 10:23:35 -0400

    Bump RETROCADE cache for launcher polish

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='retrocade-v8-scale-3x-power-scan';
+const CACHE_NAME='retrocade-v9-public-launcher-polish';
 const CORE_ASSETS=[
 './','./index.html','./admin.html','./manifest.json',
 './css/arcade.css','./css/retrocade-theme.css',
